048bb859462f5b4052c6b6c065be695fe4381163
Arsene <arsenepoutsi@gmail.com>
2020-02-14 13:28:20 +0100

    site web madera

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <?php include("templates/head.php"); ?>
+</head>
+
+<body ng-app="projetMadera" data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.18/angular.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.18/angular-route.min.js"></script>
+
+  <div class="site-wrap">
+
+    <div ng-include='"templates/header.php"'></div>
+    <div ng-view></div>
+    <div ng-include='"templates/footer.php"'></div>
+
+  </div>
+
+
+  <!-- loader -->
+  <div id="loader" style="color:#a2c037;" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#ff5e15"/></svg></div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+
+  <script src="js/main.js"></script>
+  <script src="mainangular.js"></script>
+
+</body>
+
+</html>
---
+++ templates/head.php
@@ -0,0 +1,24 @@
+<title>MADERA</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="description" content="Madera">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="fonts/icomoon/style.css">
+
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/jquery-ui.css">
+<link rel="stylesheet" href="css/owl.carousel.min.css">
+<link rel="stylesheet" href="css/owl.theme.default.min.css">
+<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+<link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+<link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+<link rel="stylesheet" href="css/aos.css">
+<link href="css/jquery.mb.YTPlayer.min.css" media="all" rel="stylesheet" type="text/css">
+
+<link rel="stylesheet" href="css/style.css">
